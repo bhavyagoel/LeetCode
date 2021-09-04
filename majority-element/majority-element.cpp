9e54f500dce5fc9263ea34285f7cacd65a9e7f21
Bhavya Goel <bgoel4132@gmail.com>
2021-09-04 13:50:57 +0530

    Time: 33 ms (9.43%), Space: 19.7 MB (51.38%) - LeetHub

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -4,22 +4,7 @@ public:
         sort(arr.begin(), arr.end());
         
         int n = arr.size();
-        int check = ceil(n/2);
-        int count = 1;
-        
-        for(int i = 0 ; i <n-1; i++) {
-            if (arr[i] == arr[i+1]) {
-                count+=1;
-            }
-            else {
-                count = 1;
-            }
-            
-            if (count > check) {
-                return arr[i];
-            }
-        }
-                
-        return arr[0];
+        int check = ceil(n/2);        
+        return arr[check];
     }
 };
