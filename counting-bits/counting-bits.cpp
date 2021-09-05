cf6a0e432da083cea3742c255a39351a2efb8c87
Bhavya Goel <bgoel4132@gmail.com>
2021-09-06 03:30:13 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.14%) - LeetHub

--- counting-bits/counting-bits.cpp
+++ counting-bits/counting-bits.cpp
@@ -14,13 +14,15 @@ public:
         
         return ans;
     }
+    
+
     vector<int> countBits(int n) {
         
-        vector<int> res; 
-                
+        vector<int> res(n+1);
         for(int i = 0; i < n+1; i++) {
-            res.push_back(__builtin_popcount(i));
-        }        
+            // res.push_back(get_count(i));
+            res[i] = res[i>>1] + (i&1);
+        }
         
         return res;
     }
