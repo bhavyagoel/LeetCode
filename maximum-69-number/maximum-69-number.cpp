6e271512853039ff0e58be34d2eb1adcdf39c62a
Bhavya Goel <bgoel4132@gmail.com>
2021-09-10 14:13:03 +0530

    Time: 2 ms (7.28%), Space: 6 MB (29.69%) - LeetHub

---
+++ maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        
+        string s = to_string(num);
+        
+        for(auto &i : s) {
+            if(i == '6'){
+                i = '9';
+                break;
+            }
+        }
+        num = stoi(s);
+        return num;
+    }
+};
