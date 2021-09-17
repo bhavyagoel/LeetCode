8f91ee7c31f0b3eaebb9a4a80b59e597a10844d1
Bhavya Goel <bgoel4132@gmail.com>
2021-09-18 01:25:27 +0530

    Runtime: 4 ms, faster than 85.67%. Memory Usage: 6.8 MB, less than 99.98%.

---
+++ reverse-words-in-a-string/most-optimised.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    // function to reverse any part of string from i to j (just one word or entire string)
+    void reverseword(string &s, int i, int j){
+        while(i<j){
+          char t=s[i];
+          s[i++]=s[j];
+          s[j--]=t;
+        } 
+    }
+    
+    string reverseWords(string &s) {
+        
+        int i=0, j=0;
+        int l=0;
+        int len=s.length();
+        int wordcount=0;
+        
+        while(true){
+            while(i<len && s[i] == ' ') i++;  // skip spaces in front of the word
+            if(i==len) break;
+            if(wordcount) s[j++]=' ';
+            l=j;
+            while(i<len && s[i] != ' ') {s[j]=s[i]; j++; i++;} 
+            reverseword(s,l,j-1);                // reverse word in place
+            wordcount++;
+            
+        }
+        
+        s.resize(j);                           // resize result string
+        reverseword(s,0,j-1);                  // reverse whole string
+        
+        return s;
+    }
+};
