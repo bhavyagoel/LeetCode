f678737090dfce416f32b25a320633ffc26157a4
Bhavya Goel <bgoel4132@gmail.com>
2021-09-19 22:14:44 +0530

    Time: 4 ms (31.12%), Space: 6 MB (24.90%) - LeetHub

--- power-of-four/power-of-four.cpp
+++ power-of-four/power-of-four.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         
-        if(n == 0) {
-            return false;
+        if(n==1) {
+            return true;
         }
-        return (floor(log(n)/log(4)) == ceil(log(n)/log(4)));
+        
+        if(n%4 != 0 | n==0) {
+            return false; 
+        }
+        
+        return isPowerOfFour(n/4);
     }
 };
