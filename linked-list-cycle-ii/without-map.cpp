93c1d5b08714165228a837d7d98d4e6aa839644f
Bhavya Goel <bgoel4132@gmail.com>
2021-09-20 03:07:54 +0530

    Create without-map.cpp

---
+++ linked-list-cycle-ii/without-map.cpp
@@ -0,0 +1,21 @@
+ListNode *detectCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    
+    ListNode *slow  = head;
+    ListNode *fast  = head;
+    ListNode *entry = head;
+    
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {                      // there is a cycle
+            while(slow != entry) {               // found the entry location
+                slow  = slow->next;
+                entry = entry->next;
+            }
+            return entry;
+        }
+    }
+    return NULL;                                 // there has no cycle
+}
