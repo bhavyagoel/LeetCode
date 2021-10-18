7100da18d5501c44e4b0d6d757577937bbec238c
Bhavya Goel <bgoel4132@gmail.com>
2021-10-18 20:01:13 +0530

    Time: 12 ms (5.28%), Space: 13 MB (5.03%) - LeetHub

---
+++ cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, pair<int, int>> temp;
+    
+    bool isCousins(TreeNode* root, int x, int y) {
+        checkDepth(root, x, 0, root->val);
+        checkDepth(root, y, 0, root->val);
+
+        return temp[x].first==temp[y].first && temp[x].second != temp[y].second;
+        
+    }
+    
+    void checkDepth(TreeNode* root, int x, int depth, int parent){
+        if(root == NULL){
+            return;
+        }
+        
+        temp[root->val] = make_pair(depth, parent);
+        
+        checkDepth(root->right, x, depth+1, root->val);
+        checkDepth(root->left, x, depth+1, root->val);
+    }
+};
