ea32d337cde3581d6c4f2a3c7b3eb645ef0ff654
Bhavya Goel <bgoel4132@gmail.com>
2021-10-19 00:44:30 +0530

    Time: 8 ms (14.31%), Space: 10.8 MB (98.82%) - LeetHub

--- cousins-in-binary-tree/cousins-in-binary-tree.cpp
+++ cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -9,15 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    unordered_map<int, pair<int, int>> temp;
+    pair<int, int> temp;
     
     bool isCousins(TreeNode* root, int x, int y) {
         checkDepth(root, x, 0, root->val);
+        pair<int, int> alphaX = temp;
+        
         checkDepth(root, y, 0, root->val);
+        pair<int, int> alphaY = temp;
 
-        return temp[x].first==temp[y].first && temp[x].second != temp[y].second;
+        return alphaX.first==alphaY.first && alphaX.second != alphaY.second;
         
     }
     
@@ -26,9 +30,37 @@ public:
             return;
         }
         
-        temp[root->val] = make_pair(depth, parent);
+        if(root->val == x){
+            temp.first = depth; 
+            temp.second = parent;
+            return;
+        }
         
         checkDepth(root->right, x, depth+1, root->val);
         checkDepth(root->left, x, depth+1, root->val);
     }
 };
+
+// class Solution {
+// public:
+//     unordered_map<int, pair<int, int>> temp;
+    
+//     bool isCousins(TreeNode* root, int x, int y) {
+//         checkDepth(root, x, 0, root->val);
+//         checkDepth(root, y, 0, root->val);
+
+//         return temp[x].first==temp[y].first && temp[x].second != temp[y].second;
+        
+//     }
+    
+//     void checkDepth(TreeNode* root, int x, int depth, int parent){
+//         if(root == NULL){
+//             return;
+//         }
+        
+//         temp[root->val] = make_pair(depth, parent);
+        
+//         checkDepth(root->right, x, depth+1, root->val);
+//         checkDepth(root->left, x, depth+1, root->val);
+//     }
+// };
