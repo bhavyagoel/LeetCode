c7992c840e0d57ed25dc06bb3913fd02273c93b4
Bhavya Goel <bgoel4132@gmail.com>
2021-10-29 23:08:33 +0530

    Time: 956 ms (5.03%), Space: 264.1 MB (5.26%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,14 +5,13 @@ public:
         for(int i = 0 ; i < s.length(); i++) {
             map<char, int> temp; 
             int len = 0; 
-            for(int j = i ; j < s.length(); j++) {
+            for(int j = i; j < s.length(); j++) {
                 if(temp[s[j]] == 0) {
                     len++; 
                     temp[s[j]] = 1;
                 }else {
                     break;
                 }
-                
             }
             max_len = max(max_len, len);
         }
