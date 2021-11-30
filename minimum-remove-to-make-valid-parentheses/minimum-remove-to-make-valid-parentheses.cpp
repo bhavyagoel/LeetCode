d74b8e144b071ef3b230cc1e57e928a093588595
Bhavya Goel <bgoel4132@gmail.com>
2021-11-30 14:32:41 +0530

    Time: 24 ms (68.58%), Space: 11.7 MB (42.82%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        
+        int n = s.length(); 
+        stack<char> temp;
+        int count = 0; 
+        for(int i = 0; i < n; i++) {
+            if (s[i]=='('){
+                temp.push(s[i]);
+                count++;
+            }
+            if (s[i]==')') {
+                if(count==0) {
+                    s[i]='#';
+                }else{
+                    count--;
+                }
+            }
+        }
+        
+        count = 0;
+        for(int i = n-1; i >-1; i--) {
+            if (s[i]==')'){
+                temp.push(s[i]);
+                count++;
+            }
+            if (s[i]=='(') {
+                if(count==0) {
+                    s[i]='#';
+                }else{
+                    count--;
+                }
+            }
+        }
+        
+        string res;
+        for(int i = 0; i < n; i++) {
+            if(s[i]!='#'){
+                res+=s[i];
+            }
+        }
+        return res; 
+    }
+};
