433e39d75c15d6156d56b3024c05a53d77a84b2e
Bhavya Goel <bgoel4132@gmail.com>
2021-12-14 18:09:05 +0530

    Time: 125 ms (43.45%), Space: 64.5 MB (92.19%) - LeetHub

--- range-sum-of-bst/range-sum-of-bst.cpp
+++ range-sum-of-bst/range-sum-of-bst.cpp
@@ -9,14 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+static auto _ = [] ()
+{ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
+
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         
         if(!root) return 0;
-        
         int val = (root->val>=low and root->val<=high)?root->val:0;
-        
         return val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low,high);
         
     }
