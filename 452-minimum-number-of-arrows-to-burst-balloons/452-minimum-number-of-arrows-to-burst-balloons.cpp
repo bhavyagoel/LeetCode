4c79e240f987817b3b3eaafe366fc52881e7a6ef
Bhavya Goel <bgoel4132@gmail.com>
2022-01-13 10:32:23 +0530

    Time: 452 ms (47.97%), Space: 89.6 MB (92.23%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -15,7 +15,6 @@ public:
         for(int i = 0; i < n; i++) {
             start = points[i][1];
             while(i<n-1 and start>=points[i+1][0]) i++;
-            // i--;
             res++;
         }
         
