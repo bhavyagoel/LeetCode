75500e22bfda460ac361d0bc84ee0c75c777fc40
Bhavya Goel <bgoel4132@gmail.com>
2022-01-16 20:52:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (44.60%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -3,10 +3,17 @@ public:
     int totalNQueens(int n) {
         string s(n, '.');
         vector<string> board(n, s);
-        return nqueen(0, board, n);
+        
+        vector<bool> sameRow(n, false);
+        vector<bool> mainDiag(2*n-1, false);
+        vector<bool> crossDiag(2*n-1, false);
+        
+        return nqueen(0, board, n, sameRow, mainDiag, crossDiag);
+        
     }
     
 private:
+    /**
     bool isSafe(int row, int col, vector<string>& board, int n) {
         
         int dupRow = row; 
@@ -35,17 +42,34 @@ private:
         
         return true; 
     }        
+    **/
     
-    int nqueen(int col, vector<string>& board, int n) {
+     int nqueen(int col, vector<string>& board, int n, vector<bool> &sameRow, vector<bool>& mainDiag, vector<bool> &crossDiag) {
         if(col==n) {
             return 1;
         }
         int count = 0; 
         for(int row = 0; row < n; row++) {
+            /**
             if(isSafe(row, col, board, n)) {
                 board[row][col]='Q';
                 count += nqueen(col+1, board, n);
                 board[row][col]='.';
+            }
+            **/
+            int mainD = row+col; 
+            int crossD = (n-1)+ (col-row);
+            
+            if(!sameRow[row] and !mainDiag[mainD] and !crossDiag[crossD]) {
+                board[col][row]='Q';
+                sameRow[row] = true; 
+                mainDiag[mainD] = true; 
+                crossDiag[crossD] = true; 
+                count += nqueen(col+1, board, n, sameRow, mainDiag, crossDiag);
+                board[col][row]='.';
+                sameRow[row] = false; 
+                mainDiag[mainD] = false; 
+                crossDiag[crossD] = false; 
             }
         }
         
