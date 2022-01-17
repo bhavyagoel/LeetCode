572b81e05108d127ce5bd8f097841de2be62922e
Bhavya Goel <bgoel4132@gmail.com>
2022-01-18 01:21:48 +0530

    Time: 3 ms (90.73%), Space: 7.7 MB (55.63%) - LeetHub

--- 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.cpp
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.cpp
@@ -11,13 +11,15 @@ public:
         for(int i = 0; i < 26; i++) {
             if(count[i].size()>0) {
                 int len = count[i].size(); 
+                // initialised cnt by 1, because initially one element exists
                 int cnt = 1;
                 int cnt1 = 0;
                 int mx = 0;
                 for(int j = 1; j < len; j++) {
-                    
+                    // Here checking for continous string
                     if((count[i][j-1]+1)==count[i][j]) ++cnt;
                     else {
+                        // check if only one different character in betweem, if so we can also add the length of second partition
                         cnt1 = (count[i][j-1]+2)==count[i][j]?cnt:0;
                         cnt = 1;
                     }
@@ -25,6 +27,7 @@ public:
                     mx = max(mx, cnt+cnt1);
     
                 }
+                // If the size of mx is less than index of this alphabet size, then there definitely exists one more element with which we can swap
                 res = max(res, mx+(count[i].size()>mx?1:0));
             }
         }
