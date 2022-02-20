6f959b2be6fb7ca7ff72d023335b38a4b5ca6e15
Bhavya Goel <bgoel4132@gmail.com>
2022-02-20 23:18:27 +0530

    Time: 26 ms (67.98%), Space: 11.3 MB (89.47%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
+/*
     static bool compare(vector<int> a, vector<int>b) {
         return (a[1]-a[0])>(b[1]-b[0]);
     }
     
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         
-        sort(intervals.begin(), intervals.end(), compare);
-        
-        // for(auto it : intervals) {
-        //     cout<<it[0]<<" "<<it[1]<<endl; 
-        // }
-        
+        sort(intervals.begin(), intervals.end(), compare);        
         int n = intervals.size(); 
         int k = 0; 
         vector<bool>check(n, false);
@@ -20,7 +16,7 @@ public:
             int start = intervals[i][0]; 
             int end = intervals[i][1]; 
             for(int j = i+1; j < n; j++) {
-                if(intervals[j][0]>=start and intervals[j][0]<end and intervals[j][1]>start and intervals[j][1]<=end and !check[j]) {
+                if(intervals[j][0]>=start and intervals[j][1]<=end and !check[j]) {
                     k++; 
                     check[j] = true; 
                 }
@@ -28,5 +24,29 @@ public:
         }
         
         return n-k; 
+    }
+*/
+    
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0]==b[0] ? a[1]>b[1] : a[0]<b[0];
+        });
+        
+        int n = intervals.size(); 
+        
+        int curr = 0; 
+        int nxt = 1; 
+        
+        int k = 0; 
+        while(curr<n and nxt<n) {
+            if(intervals[curr][0]<=intervals[nxt][0] and intervals[curr][1]>=intervals[nxt][1]) {
+                k++;
+                nxt++;
+            }
+            else if(nxt==++curr) nxt++;
+        }
+        
+        return n-k;
     }
 };
