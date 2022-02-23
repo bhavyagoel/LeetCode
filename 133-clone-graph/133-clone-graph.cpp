4196b12e7ea30c0968c1943156ab38898f37a0c1
Bhavya Goel <bgoel4132@gmail.com>
2022-02-23 12:34:22 +0530

    Time: 11 ms (35.55%), Space: 8.6 MB (71.30%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,67 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, bool> visited; 
+    unordered_map<Node*, Node*> copy; 
+    
+    Node* cloneGraph(Node* node) {
+        if(!node) return nullptr;
+        
+        queue<Node*> bfs; 
+        bfs.push(node);
+        
+        visited[node] = true; 
+        
+        Node* res; 
+        
+        while(!bfs.empty()) {
+            int curr = bfs.size(); 
+            
+            while(curr--){
+                Node* here = bfs.front(); 
+                bfs.pop(); 
+                vector<Node*> insert; 
+                
+                if(!copy[here]) {
+                    copy[here] = new Node(here->val); 
+                }
+                
+                for(auto it : here->neighbors) {
+                    if(!copy[it]) copy[it] = new Node(it->val);
+                    
+                    copy[here]->neighbors.push_back(copy[it]);                        
+                }
+                    
+                for(auto it : here->neighbors) {
+                    if(!visited[it]) {
+                        bfs.push(it);
+                        visited[it] = true;
+                    }
+                }   
+            }
+        }
+        
+        return copy[node]; 
+    }
+    
+};
