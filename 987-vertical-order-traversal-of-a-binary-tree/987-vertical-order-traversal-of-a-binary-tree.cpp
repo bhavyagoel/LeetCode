d192c27390b97f21d62124bccc14a0c3f3e78ba5
Bhavya Goel <bgoel4132@gmail.com>
2022-03-02 16:07:50 +0530

    Time: 14 ms (15.78%), Space: 13.7 MB (35.36%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -22,12 +22,32 @@ public:
         inorder(root->right, lvl+1, xAxis+1);
     }
     
-    
+    void bfs(TreeNode* root) {
+        
+        queue<pair<TreeNode*, pair<int, int>>> q; 
+        q.push({root, {0,0}});
+        
+        while(!q.empty()) {
+            int curr = q.size(); 
+            while(curr--) {
+                TreeNode* temp = q.front().first; 
+                int lvl = q.front().second.first; 
+                int xAxis = q.front().second.second; 
+                q.pop(); 
+                traversal[xAxis][lvl].insert(temp->val);
+                
+                if(temp->left) q.push({temp->left, {lvl+1, xAxis-1}});
+                if(temp->right) q.push({temp->right, {lvl+1, xAxis+1}});
+            }
+        }
+        
+        return;
+    }
     
     
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        inorder(root, 0, 0); 
-        
+        // inorder(root, 0, 0); 
+        bfs(root);
         vector<vector<int>> res; 
         for(auto xAxis : traversal) {
             vector<int> temp; 
