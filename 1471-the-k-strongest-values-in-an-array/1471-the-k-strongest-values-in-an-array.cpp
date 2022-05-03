5f4ebc1bbd0f93b2a0ec38e26aa643a824a47143
Bhavya Goel <bgoel4132@gmail.com>
2022-05-03 15:41:57 +0530

    Time: 464 ms (25.27%), Space: 80.3 MB (88.06%) - LeetHub

---
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
@@ -0,0 +1,29 @@
+struct Local{
+    Local(int median) { this->median = median; }
+    bool operator () (int a, int b) { 
+        int alpha = (a-median)>0?(a-median):(median-a); 
+        int beta = (b-median)>0?(b-median):(median-b); 
+        
+        if(alpha==beta) return a>b; 
+        else if(alpha > beta) return true; 
+        else return false; 
+    }
+
+    int median;
+};
+
+
+class Solution {
+private:
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end()); 
+        int n = arr.size(); 
+        vector<int> res(k, 0); 
+        
+        int median = arr[(n-1)/2];
+        sort(arr.begin(), arr.end(), Local(median));
+        for(int i = 0; i < k; i++) res[i] = arr[i];
+        return res;
+    }
+};
