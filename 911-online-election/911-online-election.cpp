824ff7c5d583d3a71bb15d0a13252a0367425788
Bhavya Goel <bgoel4132@gmail.com>
2022-05-24 15:49:18 +0530

    Time: 713 ms (5.11%), Space: 104.2 MB (64.05%) - LeetHub

---
+++ 911-online-election/911-online-election.cpp
@@ -0,0 +1,44 @@
+class TopVotedCandidate {
+public:
+    map<int, int> votes; 
+    vector<pair<int, int>> winner; 
+    int currWinner = -1; 
+    TopVotedCandidate(vector<int>& p, vector<int>& t) {
+        int n = p.size(); 
+        for(int i = 0; i < n; i++) {
+            votes[p[i]]++;
+            if(votes[p[i]] >= votes[currWinner]) currWinner = p[i];
+            winner.push_back({t[i], currWinner});
+        }
+        
+        for(auto it : winner) {
+            cout<<it.first<<" : "<<it.second<<endl;
+        }
+    }
+    
+    int q(int t) {
+        int hi = winner.size(), lo = 0; 
+        
+        while(lo < hi) {
+            int mid = (hi + lo)/2;
+            
+            if(t < winner[mid].first) hi = mid;  
+            else {
+                // cout<<"here"<<endl;
+                lo = mid+1; 
+            }
+        }
+        if(hi>=winner.size()) hi = winner.size()-1; 
+        cout<<hi<<" "<<lo<<endl;
+        if(winner[hi].first > t) hi>0?hi--:0;
+        
+        
+        return winner[hi].second; 
+    }
+};
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * TopVotedCandidate* obj = new TopVotedCandidate(persons, times);
+ * int param_1 = obj->q(t);
+ */
