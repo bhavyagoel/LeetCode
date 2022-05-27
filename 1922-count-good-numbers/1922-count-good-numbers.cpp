93bf74ef90809262fafc3e285019ad1ea2918e10
Bhavya Goel <bgoel4132@gmail.com>
2022-05-27 20:12:32 +0530

    Time: 5 ms (15.22%), Space: 5.9 MB (84.78%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,34 @@
+#define ll long long
+
+class Solution {
+public:
+    // evens  = 0, 2, 4, 6, 8  => 5 evens
+    // primes = 2, 3, 5, 7     => 4 primes
+  
+    int p = 1e9 + 7;
+    
+    // calculating x^y efficeiently
+    ll power(long long x, long long y) {
+      long long res = 1;    
+
+      x = x % p; // Update x if it is more than or equal to p
+      if (x == 0) return 0; 
+
+      while (y > 0) {
+        // If y is odd, multiply x with result
+        if (y & 1) res = (res*x) % p;
+        
+        // we have did the odd step is it was odd, now do the even step
+        y = y>>1; // dividing y by 2, y>>1 is same as y/2
+        x = (x*x) % p;
+      }
+      return res;
+    }
+  
+    int countGoodNumbers(long long n) {
+      ll count_of_4s = n/2;
+      ll count_of_5s = n - count_of_4s;
+      ll ans = ((power(4LL, count_of_4s) % p  * power(5LL, count_of_5s) % p) % p);
+      return (int)ans;
+    }
+};
