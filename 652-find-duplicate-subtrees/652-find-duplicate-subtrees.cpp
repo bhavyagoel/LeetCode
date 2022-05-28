d242f2ab60c93a40ac9b82f5f6235a50870ffe67
Bhavya Goel <bgoel4132@gmail.com>
2022-05-28 22:11:39 +0530

    Time: 116 ms (5.58%), Space: 46.8 MB (61.10%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -12,39 +12,25 @@
 class Solution {
 public:
     vector<TreeNode*> res; 
-    map<string, TreeNode*> check;
-    map<string, bool> dupCheck;
+    map<string, vector<TreeNode*>> check;
     
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        dfs(root); 
+        dfsSerialise(root); 
+        for(auto it : check) {
+            if(it.second.size()>1) {
+                cout<<it.first<<endl;
+                res.push_back(it.second[0]);
+            }
+        }
         return res; 
     }
     
-    void dfs(TreeNode* root) {
-        if(!root) return;
-        
-        string temp = "";
-        dfsSerialise(root, temp);
-        
-        if(check[temp] and !dupCheck[temp]) {
-            res.push_back(root);
-            dupCheck[temp] = true;
-        }
-        check[temp] = root;
-        dfs(root->left);
-        dfs(root->right);
-    }
     
-    void dfsSerialise(TreeNode* root, string &res) {
-        if(!root) {
-            res += "null,"; 
-            return;
-        }
-        
-        res += to_string(root->val);
-        res += ',';
-        dfsSerialise(root->left, res);
-        dfsSerialise(root->right, res);
-        return;
+    string dfsSerialise(TreeNode* root) {
+        if(!root) return "";
+        // brackets are added to define the subtrees uniqueness
+        string ser = "(" + dfsSerialise(root->left) + to_string(root->val) + dfsSerialise(root->right) + ")";
+        check[ser].push_back(root);
+        return ser;
     }
 };
