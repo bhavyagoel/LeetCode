f61d411946c6a34cc9e0e8f8663350c1fd5fe5cd
Bhavya Goel <bgoel4132@gmail.com>
2022-05-28 22:12:14 +0530

    Time: 104 ms (8.51%), Space: 46.8 MB (60.92%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -34,3 +34,46 @@ public:
         return ser;
     }
 };
+
+
+/*
+class Solution {
+public:
+    vector<TreeNode*> res; 
+    map<string, TreeNode*> check;
+    map<string, bool> dupCheck;
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        dfs(root); 
+        return res; 
+    }
+    
+    void dfs(TreeNode* root) {
+        if(!root) return;
+        
+        string temp = "";
+        dfsSerialise(root, temp);
+        
+        if(check[temp] and !dupCheck[temp]) {
+            res.push_back(root);
+            dupCheck[temp] = true;
+        }
+        check[temp] = root;
+        dfs(root->left);
+        dfs(root->right);
+    }
+    
+    void dfsSerialise(TreeNode* root, string &res) {
+        if(!root) {
+            res += "null,"; 
+            return;
+        }
+        
+        res += to_string(root->val);
+        res += ',';
+        dfsSerialise(root->left, res);
+        dfsSerialise(root->right, res);
+        return;
+    }
+};
+*/
