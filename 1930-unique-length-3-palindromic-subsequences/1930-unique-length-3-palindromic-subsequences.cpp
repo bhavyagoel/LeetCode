dcbecdccc06bb20c432bce6785d6fdb75637a3a3
Bhavya Goel <bgoel4132@gmail.com>
2022-05-29 12:02:28 +0530

    Time: 605 ms (43.52%), Space: 151.8 MB (10.04%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
@@ -34,6 +35,23 @@ public:
                 if(unq[j]) unq[j] |= (1 << (s[i]-'a'));
             }
         }
+        return res;
+    }
+};
+*/
+
+// Can be solved by calculating first and last occurence of each alphabet, and then counting the unique alphabets between those
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int first[26] = {[0 ... 25] = INT_MAX}, last[26] = {}, res = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
+            last[s[i] - 'a'] = i;
+        }
+        for (int i = 0; i < 26; ++i)
+            if (first[i] < last[i])
+                res += unordered_set<char>(begin(s) + first[i] + 1, begin(s) + last[i]).size();
         return res;
     }
 };
