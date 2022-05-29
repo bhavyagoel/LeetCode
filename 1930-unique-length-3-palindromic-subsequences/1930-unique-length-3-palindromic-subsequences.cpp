fc64eed62b15b1c70a8f8a42a30301e276ceda2a
Bhavya Goel <bgoel4132@gmail.com>
2022-05-29 11:59:00 +0530

    Time: 471 ms (55.23%), Space: 11.3 MB (86.19%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        
+        map<char, int> cnt; 
+        int res = 0; 
+        int n = s.size(); 
+        vector<int>unq(26,0);
+        bool check[26][26] = {false};
+
+        for(int i = 0; i < n; i++) {
+            unq[s[i]-'a'] |= (1 << (s[i]-'a'));
+            cnt[s[i]]++;
+            
+            while(i<n-1 and s[i]==s[i+1]) cnt[s[i++]]++;
+            if(cnt[s[i]]) {
+                int temp = unq[s[i]-'a'];
+                while(temp) {
+                    int r = __builtin_ctz(temp & -temp);
+                    temp ^= (temp & -temp); 
+                    if(!check[s[i]-'a'][r] and r != (s[i]-'a')) {
+                        res += 1;
+                        check[s[i]-'a'][r] = true;
+                    }
+                }                
+                if(cnt[s[i]] > 2 and !check[s[i]-'a'][s[i]-'a']) {
+                    res += 1;
+                    check[s[i]-'a'][s[i]-'a'] = true;
+                }
+                unq[s[i]-'a'] &= (1 << (s[i]-'a'));
+            }
+            
+            for(int j = 0; j < 26; j++) {
+                if(unq[j]) unq[j] |= (1 << (s[i]-'a'));
+            }
+        }
+        return res;
+    }
+};
