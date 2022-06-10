952213417c3d9f5ffe513d8878427a36445cd209
Bhavya Goel <bgoel4132@gmail.com>
2022-06-10 13:14:17 +0530

    Time: 8 ms (88.79%), Space: 8.4 MB (59.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+   int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+
+
+/*
+class Solution {
+public:
     int lengthOfLongestSubstring(string s) {
         int max_len = 0;
         for(int i = 0 ; i < s.length(); i++) {
@@ -19,3 +36,4 @@ public:
         return max_len;
     }
 };
+*/
