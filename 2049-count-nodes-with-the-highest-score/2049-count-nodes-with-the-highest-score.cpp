fb1680eccae10a5f288af42f19b00f927b6dd957
Bhavya Goel <bgoel4132@gmail.com>
2022-06-12 20:10:40 +0530

    Time: 819 ms (14.04%), Space: 168.1 MB (15.43%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
@@ -1,9 +1,37 @@
 class Solution {
 public:
     unordered_map<int, vector<int>> child;
+    int res = 0; 
+    long long maxScore = INT_MIN;
+    
+    int countHighestScoreNodes(vector<int>& p) {
+        
+        for(int i = 0; i < p.size(); i++) {
+            if(p[i]!=-1) child[p[i]].push_back(i);
+        }
+        countSubtree(p, 0);
+        return res;
+    }
+    
+    
+    int countSubtree(vector<int>& p, int curr) {
+        
+        long long sum = 1, prod = 1; 
+        for(int i = 0; i < child[curr].size(); i++) {
+            int cnt = countSubtree(p, child[curr][i]);      
+            prod *= cnt; 
+            sum += cnt; 
+        }
+        long long currScore = prod*(max(1ll, (long long)p.size() - sum));
+        if(maxScore < currScore) {
+            maxScore = currScore;
+            res = 1;
+        }
+        else if(maxScore == currScore) res += 1;
+        return sum; 
+    }
+    /*
     unordered_map<int, int> score; 
-    
-    
     int countHighestScoreNodes(vector<int>& p) {
         
         for(int i = 0; i < p.size(); i++) {
@@ -41,4 +69,6 @@ public:
         score[curr] = sum;
         return sum; 
     }
+    */
+    
 };
