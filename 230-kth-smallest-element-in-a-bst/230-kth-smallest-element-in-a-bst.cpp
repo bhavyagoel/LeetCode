7ec9565c9a947aa86c69f1bcc42209ec17a1d771
Bhavya Goel <bgoel4132@gmail.com>
2022-06-20 23:07:03 +0530

    Time: 15 ms (95.21%), Space: 24 MB (86.76%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -12,9 +12,38 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        dfs(root, k); 
+        // dfs(root, k); 
+        morrisTraversal(root, k);
         return res;
     }
+    
+    void morrisTraversal(TreeNode* root, int k) {
+        
+        while(root){
+            if(!root->left){
+                cnt++;
+                if(cnt == k) res = root->val; 
+                root = root->right; 
+            }else{
+                TreeNode* prev = root->left; 
+                
+                while(prev->right and prev->right != root) prev = prev->right; 
+                
+                if(!prev->right) {
+                    prev->right = root;
+                    root = root->left; 
+                }else {
+                    cnt++;
+                    if(cnt == k) res = root->val; 
+                    root = root->right; 
+                    prev->right = nullptr; 
+                }
+            }
+        }
+    }
+    
+    
+    
     int res = -1, cnt = 0; 
     void dfs(TreeNode* root, int k) {
         if(!root) return;
