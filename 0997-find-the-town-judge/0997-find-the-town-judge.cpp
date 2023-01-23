74a7a7d82827549de367bba5835accce7061bf3e
Bhavya Goel <bgoel4132@gmail.com>
2023-01-23 15:16:21 +0530

    Time: 408 ms (22.33%), Space: 60.7 MB (86.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int findJudge(int N, vector<vector<int>>& trust) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        unordered_map<int, int> hash;
-        for(int i=1; i<=N; i++) hash[i] = 0;
-        for(auto &v : trust){
-            auto it = hash.find(v[1]);
-            if(it != hash.end()) it->second++;
-            hash.erase(v[0]);
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> a(n, 0);
+        vector<int> b(n, 0);
+        for (auto& t : trust) {
+            ++a[t[0] - 1];
+            ++b[t[1] - 1];
         }
-        for(auto &e : hash)
-            if(e.second == N-1) return e.first;
-        
+        for (int i = 0; i < n; ++i) {
+            if ((a[i] == 0) && (b[i] == (n - 1))) return (i + 1);
+        }
         return -1;
     }
 };
